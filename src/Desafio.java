4b099047ea8faccc4aaee572648dcc7c4977c661
Bárbara Guimarães <bguima99@outlook.com>
2025-11-10 01:16:38 -0300

    Desafio concluído

---
+++ src/Desafio.java
@@ -0,0 +1,15 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Desafio {
+    public static void main(String[] args) {
+        String nome = "Bárbara Guimarães";
+        String tipoConta = "Corrente";
+        double saldo = 1518;
+
+        System.out.println("------------------------");
+        System.out.println("\nNome do cliente: " +nome);
+        System.out.println("Tipo da conta: " + tipoConta);
+        System.out.println("Saldo disponível: " + saldo);
+        System.out.println("\n------------------------");
+    }
+}
